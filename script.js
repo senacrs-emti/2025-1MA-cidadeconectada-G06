1e5685cf016f13a81a073cfe4609e2d5450e820b
Joana Nonemacher <joananonemacher9@gmail.com>
2025-11-06 10:11:41 -0300

    Pequenas modificações
    adição da imagem do sobre nos

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
- 
+ a fazer: 
+ adicionar pipa
+ texto vivi arredondado
+ fundos dos blocos
+ carrossel
+ footer do zero
+ border na foto sobre nos
+ cabeçalho centralizar horizontalmente
+ favicon
+ img e sobre lado a lado (width da img)
